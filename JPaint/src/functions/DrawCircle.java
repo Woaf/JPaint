c8dd789505995eb196ca161ad79f2ec1b603020f
Woaf <bfazekas@inf.elte.hu>
2017-03-19 18:57:04 +0100

    cleared license template comments

--- JPaint/src/functions/DrawCircle.java
+++ JPaint/src/functions/DrawCircle.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package functions;
 
-/**
- *
- * @author balin
- */
 public class DrawCircle {
     // az egér kezdeti es vegpozicio közötti tavolsagu kor kirajzolasa
 }
